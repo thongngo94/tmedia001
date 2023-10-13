394a8fd3fec8a9b3f8ad430273450a93da29a846
thongngo94 <146570110+thongngo94@users.noreply.github.com>
2023-10-13 18:43:05 +0700

    Update FacebookView.vue

--- src/pages/Facebook/FacebookView.vue
+++ src/pages/Facebook/FacebookView.vue
@@ -469,7 +469,7 @@
               Bảng Báo Giá Dịch Vụ Quảng Cáo Facebook</h2>
 
             <p class="section-text">
-              Dưới đây là bảng báo giá dịch vụ quảng cáo Facebook tại T-Media. Quý khách hàng có nhu cầu tìm hiểu hoặc tư vấn về dịch vụ, vui lòng liên hệ hotline 0935 7777 69 để nhận hỗ trợ của chuyên viên marketing. Trân trọng
+              Dưới đây là bảng báo giá dịch vụ quảng cáo Facebook tại T-Media. Quý khách hàng có nhu cầu tìm hiểu hoặc tư vấn về dịch vụ, vui lòng liên hệ hotline 0964.898.041 để nhận hỗ trợ của chuyên viên marketing. Trân trọng
             </p>
 
             <ul class="grid-list">
